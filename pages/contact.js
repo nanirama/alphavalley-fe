6d771a4156c37935e1b0b985be909c00f29090d8
nanirama <nani.rama1982@gmail.com>
2022-09-19 12:49:53 +0530

    Contact page done3

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Layout from "../components/layout"
 import Seo from '../components/seo'
 
@@ -7,13 +8,22 @@ import Contactinfo from "../components/contact/contactinfo"
 export default function Contact({ }) {
     return (
         <Layout>
-            <div className="contact-us">
-                <div className="container">
-                    <Contactform />
-                    <Contactinfo />
+            <GoogleReCaptchaProvider
+                reCaptchaKey="6LfLFw4iAAAAAKj0H7iflRMk8ymVcKUwWYor-Eia"
+                scriptProps={{
+                    async: false,
+                    defer: false,
+                    appendTo: "head",
+                    nonce: undefined,
+                }}
+            >
+                <div className="contact-us">
+                    <div className="container">
+                        <Contactform />
+                        <Contactinfo />
+                    </div>
                 </div>
-            </div>
-
+            </GoogleReCaptchaProvider>
         </Layout>
     )
 }
