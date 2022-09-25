ec4102577203760821b14fad43cef9e6f6ef6318
nanirama <nani.rama1982@gmail.com>
2022-09-25 16:00:29 +0530

    speed 8

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com','images.unsplash.com'],
-    formats: ['image/avif', 'image/webp']
+    formats: ['image/webp']
   },  
 }
 
