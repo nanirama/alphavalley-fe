b04d54a3bb689faa53b2689cf230233faa03f93f
nanirama <nani.rama1982@gmail.com>
2022-10-10 12:17:33 +0530

    Footerlogo changed

--- components/Pitchdeck/Herosection.js
+++ components/Pitchdeck/Herosection.js
@@ -15,9 +15,8 @@ export default function Herosection() {
                         <p>We create compelling, investor-ready pitch decks for startups.</p>
                         {/* <button className="btn">Contact us</button> */}
                         <div className="my-4 py-2">
-                        <LandingPageForm subject="Pitch Deck Enquiry - UAE"/>
+                        <LandingPageForm subject="Pitch Deck Enquiry"/>
                         </div>
-                        {/* <iframe width='110%' height='450px' className="overflow-hidden" src='https://bigin.zoho.in/crm/WebFormServeServlet?rid=f1a1faee80712dc9c0d45324653a5bd02198df88165532d85a64ab75dc0680e8gid6cd952f3e5be67584d0644fe79b8ff8f467182910d36d1784d783e5c92f37855'></iframe> */}
                      </div>
                 
              
